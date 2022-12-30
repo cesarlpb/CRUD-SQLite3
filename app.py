74afb125bfed668c70064616093ea1c26b2f3201
César P <cesarlpb@gmail.com>
2022-12-30 15:06:05 +0000

    Añadimos conexión a MySQL

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 # app.py
 
 from flask import Flask
+from flaskext.mysql import MySQL
 
 app = Flask(__name__) # application 'app' is object of class 'Flask'
+mysql = MySQL()
 
-# import files
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'MySQL123/'
+app.config['MYSQL_DATABASE_DB'] = 'test_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
+# To use MySQL, we need a cursor:
+    # Creating a connection cursor
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+# To create a table:
+table_name = "test" # <--- Change table name
+table_definition = "(Id INT AUTO_INCREMENT PRIMARY KEY, Question VARCHAR(255), Answer VARCHAR(255))"
+table_columns = f"(Question, Answer)"
+table_values = f"('What is your name?', 'John')"
+
+print(f"CREATE TABLE IF NOT EXISTS {table_name} {table_definition}")
+cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} {table_definition}")
+cursor.execute(f"INSERT INTO {table_name} {table_columns} VALUES {table_values}")
+
+#Closing the cursor
+cursor.close()
+conn.close()
+
+# import routes
 from routes import *
 
 
